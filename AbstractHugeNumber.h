0cbfcf1eb1998f325affd924ed9e42a19f71438f
Eitan Cherkasov <102179966+EitanCher@users.noreply.github.com>
2024-04-09 22:16:57 +0300

    Add files via upload

---
+++ AbstractHugeNumber.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <string>
+using namespace std;
+class AbstractHugeNumber
+{
+protected : 
+	string hugeNumAsString;
+public:
+	virtual double Convert() = 0; // return stod(hugeNumAsString);
+	virtual operator double() = 0;
+	
+	virtual AbstractHugeNumber* operator+(AbstractHugeNumber& other) = 0;
+	virtual operator string() {	return GetStringAsNumber();	}
+	virtual string& GetStringAsNumber()	{ return hugeNumAsString; }
+};
