8f8d2e538999a42bc282d5315b683c24b6f7024e
Eitan Cherkasov <102179966+EitanCher@users.noreply.github.com>
2024-04-10 10:42:14 +0300

    Update FractHugeNumber.h

--- FractHugeNumber.h
+++ FractHugeNumber.h
@@ -5,5 +5,7 @@ class FractHugeNumber : public AbstractHugeNumber
 {
 public:
     FractHugeNumber(const string& numString);
-    
+    double Convert() override;
+    operator double() override;
+    AbstractHugeNumber* operator+(AbstractHugeNumber&) override;
 };
